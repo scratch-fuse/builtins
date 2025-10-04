1b56d3d7e731d1aa13ab1c61665bb91aaf4a97ab
FurryR <awathefox@gmail.com>
2025-10-04 23:57:25 +0800

    :sparkles: feat: initial commit
    Signed-off-by: FurryR <awathefox@gmail.com>

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# builtins
+
+A small collection of built-in block definitions for Scratch projects.
+
+> Minimal repository containing source and type declarations used by general Scratch projects.
+
+# License
+
+MPL-2.0. See [LICENSE](./LICENSE) for details.
---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import tsup from 'tsup'
+
+export default tsup.defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  sourcemap: false,
+  clean: true,
+  minify: false,
+  target: 'esnext',
+  outDir: 'dist',
+  loader: {
+    '.fuse': 'text'
+  }
+})
